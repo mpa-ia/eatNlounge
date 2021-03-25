13ee40f463d47f1e6070a8e56c0e30bec81f2b66
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-03-25 21:21:45 +0100

    add logout function in user context

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { content } from '../../settings';
 import { useUser } from '../../context/user';
+import { Button } from 'antd';
 const Nav: React.FunctionComponent = () => {
-  const { userData } = useUser();
+  const { userData, logout } = useUser();
   return (
     <nav>
       {userData ?
-        <span>{userData.name} {userData.surname}</span>
+        <>
+          <span>{userData.name} {userData.surname}</span>
+          <Button onClick={logout}>Logout</Button>
+        </>
         :
         <Link href="/signin">
           <a>{content.general.signIn}</a>
--- src/context/user.tsx
+++ src/context/user.tsx
@@ -1,15 +1,25 @@
 
 import React, { useEffect } from 'react';
 import { authorize } from '../services/auth';
+import Cookies from 'js-cookie';
 
 type SetUser = (user: User.Data | null) => void;
 type UserContextProps = {
 	userData: User.Data | null;
-	setUser: SetUser;
+  setUser: SetUser;
+  logout: () => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-const UserContext = React.createContext<UserContextProps | never>({ userData: null, setUser: () => { console.warn('initialize'); }});
+const UserContext = React.createContext<UserContextProps | never>({
+  userData: null,
+  setUser: () => {
+    console.warn('initialize');
+  },
+  logout: () => {
+    console.warn('initialize');
+  },
+});
 
 export const UserProvider: React.FC = ({ children }) => {
   const [userData, setUserData] = React.useState<User.Data | null>(null);
@@ -21,18 +31,22 @@ export const UserProvider: React.FC = ({ children }) => {
       }
     };
     authorizeUser();
-  },[]);
+  }, []);
   const setUser: SetUser = user => {
     setUserData(user);
   };
+  const logout = (): void => {
+    Cookies.remove('id_token');
+    setUser(null);
+  };
   return (
-    <UserContext.Provider value={{ userData, setUser }}>
+    <UserContext.Provider value={{ userData, setUser, logout }}>
       {children}
     </UserContext.Provider>
   );
 };
 
 export const useUser = (): UserContextProps => {
-  const { userData, setUser } = React.useContext(UserContext);
-  return { userData, setUser };
+  const { userData, setUser, logout } = React.useContext(UserContext);
+  return { userData, setUser, logout };
 };
