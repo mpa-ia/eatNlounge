7f905aec857563fb66da83410fb4ffd03b22d93f
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-04-09 20:26:18 +0200

    create Card style component, style Sign Up page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,7 +3,8 @@ import SignUpForm from '../../components/SignUpForm';
 import Link from 'next/link';
 import { content } from '../../settings';
 import { signUp } from '../../services/auth';
-
+import { Card } from '../../styles/layout.style';
+import { Col, Row } from 'antd';
 const SignUp: React.FunctionComponent = () => {
   const SignUp = async (formFieldsData: User.SignUp): Promise<void> => { 
     const res = await signUp(formFieldsData);
@@ -14,10 +15,17 @@ const SignUp: React.FunctionComponent = () => {
 
   return (
     <div>
-      <SignUpForm onSignUp={SignUp} />
-      <Link href="/signin">
-        <a>{content.pages.signUpIn.signInLink}</a>
-      </Link>
+      <Row>
+        <Col span={12} offset={6}>
+          <Card type="lightAccent" >
+            <SignUpForm onSignUp={SignUp} />
+            <Link href="/signin">
+              <a>{content.pages.signUpIn.signInLink}</a>
+            </Link>
+          </Card>
+        </Col>
+      </Row>
+
     </div>
   );
 };
---
+++ src/styles/layout.style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { variables } from './settings';
+
+type CardType = keyof typeof variables.color;
+
+interface CardProps {
+	type: CardType;
+	[key: string]: unknown;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Card = styled(({ type, ...rest }: CardProps) => <div {...rest} />)`
+	border-radius: 8px;
+	background-color: ${({ type }) => variables.color[type]};
+	color: ${({ type }) => type === 'darkShade' ? variables.color.lightShades : variables.color.darkShade};
+	padding: 1.4rem;
+	box-shadow: 3px 1px 10px 3px ${ variables.color.default };
+`;
