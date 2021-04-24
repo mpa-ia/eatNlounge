d058a3d792e8fa84712102fb0b1a000120c962e0
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-04-24 19:41:41 +0200

    display dash between hours in BookingListItem

--- src/components/BookingListItem/index.tsx
+++ src/components/BookingListItem/index.tsx
@@ -10,28 +10,36 @@ interface Props {
   onEdit: IDCallback;
   onPreview: IDCallback;
 }
-const BookingListItem: React.FunctionComponent<Props> = (props) => {
-
-  return (
-    <li>
+const BookingListItem: React.FunctionComponent<Props> = (props) =>
+  <li>
+    <Button
+      type="text"
+      onClick={props.onPreview.bind(null, props.booking._id)}
+    >
+      {TimeFormatter.parseUnixToReadableDate(props.booking.date)}
+        &nbsp;
+      {props.booking.hours.map((hour, idx) =>
+        `${TimeFormatter.parseHour(hour)}${idx !== props.booking.hours.length - 1 ? '-' : ''}`,
+      )}
+        &nbsp;
+      {props.booking.table}
+    </Button>
+    <Tooltip title="Edit">
       <Button
+        onClick={props.onEdit.bind(null, props.booking._id)}
+        shape="circle"
         type="text"
-        onClick={props.onPreview.bind(null, props.booking._id)}
-      >
-        {TimeFormatter.parseUnixToReadableDate(props.booking.date)}
-        &nbsp;
-        {props.booking.hours.map(hour => TimeFormatter.parseHour(hour))}
-        &nbsp;
-        {props.booking.table}
-      </Button>
-      <Tooltip title="Edit">
-        <Button onClick={props.onEdit.bind(null, props.booking._id)} shape="circle" type="text" icon={<EditFilled />}></Button>
-      </Tooltip>
-      <Tooltip title="Cancel">
-        <Button onClick={props.onCancel.bind(null, props.booking._id)} shape="circle" type="text" icon={<DeleteFilled />}></Button>
-      </Tooltip> 
-    </li>
-  );
-};
+        icon={<EditFilled />}
+      />
+    </Tooltip>
+    <Tooltip title="Cancel">
+      <Button
+        onClick={props.onCancel.bind(null, props.booking._id)}
+        shape="circle"
+        type="text"
+        icon={<DeleteFilled />}
+      />
+    </Tooltip>
+  </li>;
 
 export default BookingListItem;
