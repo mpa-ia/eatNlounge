3bad80babd0b9fb3d5eb030f04fc5631a3db06d3
maria.pabich.500@guest.p.lodz.pl <marysia.pabich@gmail.com>
2021-04-24 19:29:13 +0200

    return bookings with future date

--- src/pages/api/bookings/index.ts
+++ src/pages/api/bookings/index.ts
@@ -2,11 +2,13 @@ import dbConnect from '../../../../server/config/dbConnect';
 import Booking from '../../../../server/models/booking.model';
 import { Response, Request } from 'express/index';
 import { errorCodes } from '../../../settings/codes';
+import moment from 'moment';
 
 export default async function bookingHandler (req: Request, res: Response): Promise<void> {
   await dbConnect();
   try {
-    const bookings = await Booking.find();
+    const now = moment().unix();
+    const bookings = await Booking.find({ date: { '$gt': now } });
     if (!bookings) res.status(404).json({ error: true/* , errorCode: errorCodes.NO_RESOURCE */ });
     else {
       res.json({ status: 'success', data: bookings });
